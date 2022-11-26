740cd5b3b4329f60c48552a5960d89ff17b278bf
Sarah Ali <sara2018170167@cis.asu.edu.eg>
2022-11-26 23:19:47 +0200

    uploaded all

---
+++ TextStyle/dynamicStyling.js
@@ -0,0 +1,39 @@
+function ChangeFont(name){
+
+    document.getElementById("PAR").setAttribute("font-family",name);
+    document.getElementById("PAR").style.fontFamily=name;
+}
+function ChangeAlign(name){
+
+    
+    document.getElementById("PAR").style.textAlign=name;
+}
+function ChangeHeight(name){
+
+    document.getElementById("PAR").style.lineHeight=name;
+}
+function ChangeLSpace(name){
+
+    document.getElementById("PAR").style.letterSpacing=name;
+}
+
+ function ChangeIndent(name){
+
+    document.getElementById("PAR").style.textIndent=name;
+}
+ function ChangeTransform(name){
+
+    document.getElementById("PAR").style.textTransform=name;
+}
+ function ChangeDecorate(name){
+
+    document.getElementById("PAR").style.textDecoration=name;
+}
+ function ChangeBorder(name){
+
+    document.getElementById("PAR").style.border=name;
+}
+function ChangeBorderColor(name){
+
+    document.getElementById("PAR").style.borderColor=name;
+}
